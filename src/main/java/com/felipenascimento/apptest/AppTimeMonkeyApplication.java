324bb4a39a3cf323f0bba7aaa18b32cfd7f0f24a
F1n1sh3r <felipe1.jn@hotmail.com>
2018-03-15 04:08:29 -0300

    Create Project

---
+++ src/main/java/com/felipenascimento/apptest/AppTimeMonkeyApplication.java
@@ -0,0 +1,12 @@
+package com.felipenascimento.apptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTimeMonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTimeMonkeyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/felipenascimento/apptest/AppTimeMonkeyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.felipenascimento.apptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTimeMonkeyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
